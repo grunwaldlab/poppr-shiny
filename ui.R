5ba091d2fff70fb4ecc13479d27216576737ca88
Niklaus Grunwald <grunwaln@science.oregonstate.edu>
2014-07-03 14:52:54 -0700

    loaded sessions better

--- ui.R
+++ ui.R
@@ -1,26 +1,27 @@
 shinyUI(
   navbarPage("Population Genetics in R",
              theme = "bootstrap.css",
-             tabPanel("Load data"),
+             tabPanel("Load data",
+                      sidebarPanel(
+                        fileInput('file1', 'Load file',
+                                  accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
+                        tags$hr(),
+                        checkboxInput('header', 'Header', TRUE),
+                        radioButtons('sep', 'Separator',
+                                     c(Comma=',',
+                                       Semicolon=';',
+                                       Tab='\t'),
+                                     'Comma')
+                      ),
+                      mainPanel(
+                        tableOutput('contents')
+                      )
+             ),
              navbarMenu("Analysis",
                 tabPanel("Poppr", verbatimTextOutput("poppr")),
                 tabPanel("More")),
              tabPanel("Help"),
-             tabPanel("Session info", verbatimTextOutput("sessioninfo")),
-             sidebarPanel(
-               fileInput('file1', 'Load file',
-                         accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
-               tags$hr(),
-               checkboxInput('header', 'Header', TRUE),
-               radioButtons('sep', 'Separator',
-                            c(Comma=',',
-                              Semicolon=';',
-                              Tab='\t'),
-                            'Comma')
-               ),
-   mainPanel(
-            tableOutput('contents')
-            )
+             tabPanel("Session info", verbatimTextOutput("sessioninfo"))
              
       # application UI              
 ))
